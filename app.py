dcafc0f671c0c331d71a34d7c8c4b6f281d737f8
Harun Ayartürk <harunayarturk@gmail.com>
2026-02-06 02:38:37 +0100

    Initialize database

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///moviwebapp.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.get("/")
+def home():
+    return "MoviWebApp is running!"
